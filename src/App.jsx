3b3a0a5e4d03d66192a9bd41dbbe9cd6a2c3614c
Melody Cartwright <melody94c@gmail.com>
2025-02-23 21:16:01 +0100

    upaed app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,21 @@ import React, {useState} from 'react'
 import './App.css'
 
 function App() {
+  const [todos, setTodos] = useState([]);
 
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text, isCompleted: false }];
+  };
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    setTodos(newTodos);
+  };
+const removeTodo = (index) => {
+  const newTodos = [...todos];
+  newTodos.splice(index, 1);
+  setTodos(newTodos);
+};
 
   return (
     <div className='App'>
